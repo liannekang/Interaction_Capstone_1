bfdd8f4f3ab3f05267078e41aef168eed9b7f217
liannekang <36962211+liannekang@users.noreply.github.com>
2019-09-27 18:35:43 -0500

    Add files via upload

---
+++ rrepository/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+
+// $("#week1").mouseenter(function(){
+//   	$(".w1").css('fontsize', '50px');
+//  });
+
+// $("#week2").mouseenter(function(){
+//   	$(this).css('background-color', '#D6DBDF');
+//  });
+
+// $("#week3").mouseenter(function(){
+//   	$(this).css('background-color', '#AEB6BF');
+//  });
+
+// $("#week4").mouseenter(function(){
+//   	$(this).css('background-color', '#85929E');
+//  });
+
+
+
+
+
+
+
+// //end of document 
+//  });
