c24fbf7928a93366af8325f8c4deeec353fc0a4f
liannekang <36962211+liannekang@users.noreply.github.com>
2019-10-01 09:13:59 -0500

    Add files via upload

---
+++ type/js/script.js
@@ -0,0 +1,857 @@
+$(document).ready(function(){
+
+
+// $('.click').on('click',function(){
+
+// if($(".box,.click").attr('data-click-state') == 1) {
+// $(".box,.click").attr('data-click-state', 0)
+// $(".box,.click").css('background-color', '#41d4cf')
+// $("body, #alp").css('background-color', '#10206b')
+// } else {
+// $(".box,.click").attr('data-click-state', 1)
+// 	$(".box,.click").css('background-color', '#ee553b')
+// 	$("body, #alp").css('background-color', '#f6d55c')
+// }
+
+// });
+
+//-click to-//
+
+//-c-//
+$("#ct").mouseenter(function(){
+  	$(".ct1").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#ct").mouseleave(function(){
+  	$(".ct1").animate({
+			height: '80px',
+			}, 200);
+ });
+
+$("#lt").mouseenter(function(){
+  	$(".lt1").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#lt").mouseleave(function(){
+  	$(".lt1").animate({
+			height: '85px',
+			}, 200);
+ });
+
+$("#it").mouseenter(function(){
+  	$(".it1").animate({
+			width: '0px',
+			}, 200);
+  	$(".it2").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#it").mouseleave(function(){
+  	$(".it1").animate({
+			width: '40px',
+			}, 200);
+  	$(".it2").animate({
+			width: '40px',
+			}, 200);
+ });
+
+
+$("#ctt").mouseenter(function(){
+  	$(".ctt1").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#ctt").mouseleave(function(){
+  	$(".ctt1").animate({
+			height: '80px',
+			}, 200);
+ });
+
+$("#kt").mouseenter(function(){
+  	$(".kt1").animate({
+			height: '0px',
+			}, 200);
+  	$(".kt2").animate({
+			width: '0px',
+			}, 200);
+  	$(".kt3").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#kt").mouseleave(function(){
+  	$(".kt1").animate({
+			height: '25px',
+			}, 200);
+  	$(".kt2").animate({
+			width: '40px',
+			}, 200);
+  	$(".kt3").animate({
+			height: '25px',
+			}, 200);
+ });
+
+
+$("#tt").mouseenter(function(){
+  	$(".tt1").animate({
+			width: '0px',
+			}, 200);
+  	$(".tt2").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#tt").mouseleave(function(){
+  	$(".tt1").animate({
+			width: '30px',
+			}, 200);
+  	$(".tt2").animate({
+			width: '30px',
+			}, 200);
+ });
+
+$("#ot").mouseenter(function(){
+  	$(".ot1").animate({
+			width: '0px',
+			}, 200);
+  
+ });
+
+$("#ot").mouseleave(function(){
+  	$(".ot1").animate({
+			width: '60px',
+			}, 200);
+ });
+
+
+
+//-SLIDE title-//
+$("#sf").mouseleave(function(){
+  	$(".sf1").animate({
+			width: '150px',
+			}, 200);
+  	$(".sf2").animate({
+			height: '85px',
+			}, 200);
+ });
+
+$("#sf").mouseenter(function(){
+  	$(".sf1").animate({
+			width: '0px',
+			}, 200);
+  	$(".sf2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#lf").mouseenter(function(){
+  	$(".lf1").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#lf").mouseleave(function(){
+  	$(".lf1").animate({
+			height: '170px',
+			}, 200);
+ });
+
+$("#if").mouseenter(function(){
+  	$(".if1").animate({
+			width: '0px',
+			}, 200);
+  	$(".if2").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#if").mouseleave(function(){
+  	$(".if1").animate({
+			width: '80px',
+			}, 200);
+  	$(".if2").animate({
+			width: '80px',
+			}, 200);
+ });
+
+$("#df").mouseenter(function(){
+  	$(".df1").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#df").mouseleave(function(){
+  	$(".df1").animate({
+			width: '120px',
+			}, 200);
+ });
+
+$("#ef").mouseenter(function(){
+  	$(".ef1").animate({
+			width: '0px',
+			}, 370);
+  	$(".ef2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#ef").mouseleave(function(){
+  	$(".ef1").animate({
+			width: '135px',
+			}, 10);
+  	$(".ef2").animate({
+			height: '90px',
+			}, 200);
+
+});
+
+
+
+
+
+
+
+
+//-ALPHABET-//
+//-a-//
+$("#al").mouseenter(function(){
+  	$(".al1").animate({
+			width: '0px',
+			}, 200);
+  	$(".al2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#al").mouseleave(function(){
+  	$(".al1").animate({
+			width: '120px',
+			}, 200);
+  	$(".al2").animate({
+			height: '100px',
+			}, 200);
+ });
+
+
+//-b-//
+$("#bl").mouseenter(function(){
+  	$(".bl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".bl2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#bl").mouseleave(function(){
+  	$(".bl1").animate({
+			width: '120px',
+			}, 200);
+  	$(".bl2").animate({
+			height: '84px',
+			}, 200);
+ });
+
+//-c-//
+$("#cl").mouseenter(function(){
+  	$(".cl1").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#cl").mouseleave(function(){
+  	$(".cl1").animate({
+			height: '160px',
+			}, 200);
+ });
+
+//-d-//
+$("#dl").mouseenter(function(){
+  	$(".dl1").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#dl").mouseleave(function(){
+  	$(".dl1").animate({
+			width: '120px',
+			}, 200);
+ });
+
+
+
+
+//-e-//
+$("#el").mouseenter(function(){
+  	$(".el1").animate({
+			width: '0px',
+			}, 370);
+  	$(".el2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#el").mouseleave(function(){
+  	$(".el1").animate({
+			width: '135px',
+			}, 10);
+  	$(".el2").animate({
+			height: '90px',
+			}, 200);
+ });
+
+//-f-//
+$("#fl").mouseenter(function(){
+  	$(".fl1").animate({
+			width: '0px',
+			}, 370);
+  	$(".fl2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#fl").mouseleave(function(){
+  	$(".fl1").animate({
+			width: '135px',
+			}, 10);
+  	$(".fl2").animate({
+			height: '100px',
+			}, 200);
+ });
+
+//-g-//
+$("#gl").mouseenter(function(){
+  	$(".gl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".gl2").animate({
+			height: '0px',
+			}, 200);
+  	$(".gl3").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#gl").mouseleave(function(){
+  	$(".gl1").animate({
+			width: '140px',
+			}, 200);
+  	$(".gl2").animate({
+			height: '25px',
+			}, 200);
+  	$(".gl3").animate({
+			height: '80px',
+			}, 200);
+ });
+
+//-h-//
+$("#hl").mouseenter(function(){
+  	$(".hl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".hl2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#hl").mouseleave(function(){
+  	$(".hl1").animate({
+			width: '120px',
+			}, 200);
+  	$(".hl2").animate({
+			height: '90px',
+			}, 200);
+ });
+
+//-i-//
+$("#il").mouseenter(function(){
+  	$(".il1").animate({
+			width: '0px',
+			}, 200);
+  	$(".il2").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#il").mouseleave(function(){
+  	$(".il1").animate({
+			width: '80px',
+			}, 200);
+  	$(".il2").animate({
+			width: '80px',
+			}, 200);
+ });
+
+//-j-//
+$("#jl").mouseenter(function(){
+  	$(".jl1").animate({
+			width: '0px',
+			}, 400);
+  	$(".jl2").animate({
+			width: '0px',
+			}, 100);
+ });
+
+$("#jl").mouseleave(function(){
+  	$(".jl1").animate({
+			width: '80px',
+			}, 100);
+  	$(".jl2").animate({
+			width: '20px',
+			}, 200);
+ });
+
+//-k-//
+$("#kl").mouseenter(function(){
+  	$(".kl1").animate({
+			height: '0px',
+			}, 200);
+  	$(".kl2").animate({
+			width: '0px',
+			}, 200);
+  	$(".kl3").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#kl").mouseleave(function(){
+  	$(".kl1").animate({
+			height: '50px',
+			}, 200);
+  	$(".kl2").animate({
+			width: '80px',
+			}, 200);
+  	$(".kl3").animate({
+			height: '50px',
+			}, 200);
+ });
+
+
+//-l-//
+$("#ll").mouseenter(function(){
+  	$(".ll1").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#ll").mouseleave(function(){
+  	$(".ll1").animate({
+			height: '170px',
+			}, 200);
+ });
+
+//-m-//
+$("#ml").mouseenter(function(){
+  	$(".ml1").animate({
+			width: '0px',
+			}, 200);
+  	$(".ml2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#ml").mouseleave(function(){
+  	$(".ml1").animate({
+			width: '65px',
+			}, 200);
+  	$(".ml2").animate({
+			height: '175px',
+			}, 200);
+ });
+
+//-n-//
+$("#nl").mouseenter(function(){
+  	$(".nl1").animate({
+			height: '0px',
+			}, 200);
+  
+ });
+
+$("#nl").mouseleave(function(){
+  	$(".nl1").animate({
+			height: '170px',
+			}, 200);
+ });
+
+//-o-//
+$("#ol").mouseenter(function(){
+  	$(".ol1").animate({
+			width: '0px',
+			}, 200);
+  
+ });
+
+$("#ol").mouseleave(function(){
+  	$(".ol1").animate({
+			width: '120px',
+			}, 200);
+ });
+
+
+//-p-//
+$("#pl").mouseenter(function(){
+  	$(".pl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".pl2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#pl").mouseleave(function(){
+  	$(".pl1").animate({
+			width: '120px',
+			}, 200);
+  	$(".pl2").animate({
+			height: '95px',
+			}, 200);
+ });
+
+//-q-//
+$("#ql").mouseenter(function(){
+  	$(".ql1").animate({
+			height: '0px',
+			}, 200);
+  	$(".ql2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#ql").mouseleave(function(){
+  	$(".ql1").animate({
+			height: '160px',
+			}, 200);
+  	$(".ql2").animate({
+			height: '110px',
+			}, 200);
+ });
+
+
+
+
+//-r-//
+$("#rl").mouseenter(function(){
+  	$(".rl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".rl2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#rl").mouseleave(function(){
+  	$(".rl1").animate({
+			width: '120px',
+			}, 200);
+  	$(".rl2").animate({
+			height: '100px',
+			}, 200);
+ });
+
+//-s-//
+$("#sl").mouseenter(function(){
+  	$(".sl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".sl2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#sl").mouseleave(function(){
+  	$(".sl1").animate({
+			width: '150px',
+			}, 200);
+  	$(".sl2").animate({
+			height: '85px',
+			}, 200);
+ });
+
+//-t-//
+$("#tl").mouseenter(function(){
+  	$(".tl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".tl2").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#tl").mouseleave(function(){
+  	$(".tl1").animate({
+			width: '60px',
+			}, 200);
+  	$(".tl2").animate({
+			width: '60px',
+			}, 200);
+ });
+
+
+
+
+//-u-//
+$("#ul").mouseenter(function(){
+  	$(".ul1").animate({
+			width: '0px',
+			}, 200);
+  
+ });
+
+$("#ul").mouseleave(function(){
+  	$(".ul1").animate({
+			width: '120px',
+			}, 200);
+ });
+
+
+
+//-v-//
+$("#vl").mouseenter(function(){
+  	$(".vl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".vl2").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#vl").mouseleave(function(){
+  	$(".vl1").animate({
+			width: '120px',
+			}, 200);
+  	$(".vl2").animate({
+			width: '80px',
+			}, 200);
+ });
+
+
+//-w-//
+$("#wl").mouseenter(function(){
+  	$(".wl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".wl2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#wl").mouseleave(function(){
+  	$(".wl1").animate({
+			width: '65px',
+			}, 200);
+  	$(".wl2").animate({
+			height: '170px',
+			}, 200);
+ });
+
+
+//-x-//
+$("#xl").mouseenter(function(){
+  	$(".xl1").animate({
+			height: '0px',
+			}, 200);
+  	$(".xl2").animate({
+			width: '0px',
+			}, 200);
+  	$(".xl3").animate({
+			height: '0px',
+			}, 200);
+  	$(".xl4").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#xl").mouseleave(function(){
+  	$(".xl1").animate({
+			height: '90px',
+			}, 200);
+  	$(".xl2").animate({
+			width: '80px',
+			}, 200);
+  	$(".xl3").animate({
+			height: '90px',
+			}, 200);
+  	$(".xl4").animate({
+			width: '80px',
+			}, 200);
+ });
+
+//-y-//
+$("#yl").mouseenter(function(){
+  	$(".yl1").animate({
+			height: '0px',
+			}, 200);
+  	$(".yl2").animate({
+			width: '0px',
+			}, 200);
+  	$(".yl3").animate({
+			width: '0px',
+			}, 200);
+ });
+
+$("#yl").mouseleave(function(){
+  	$(".yl1").animate({
+			height: '50px',
+			}, 200);
+  	$(".yl2").animate({
+			width: '70px',
+			}, 200);
+  	$(".yl3").animate({
+			width: '70px',
+			}, 200);
+ });
+
+//-z-//
+$("#zl").mouseenter(function(){
+  	$(".zl1").animate({
+			width: '0px',
+			}, 200);
+  	$(".zl2").animate({
+			height: '0px',
+			}, 200);
+ });
+
+$("#zl").mouseleave(function(){
+  	$(".zl1").animate({
+			width: '150px',
+			}, 200);
+  	$(".zl2").animate({
+			height: '85px',
+			}, 200);
+ });
+
+
+
+//-on click-//
+
+
+
+$(".large").mousedown(function(){
+		$('.large').animate({marginLeft: "23%"},600);
+        $('.box').animate({marginLeft: "0px"},600);
+        $('.wrapper').animate({marginTop: "0px"},600);
+});
+
+$(".large").mouseup(function(){
+		$('.large').animate({marginLeft: "13%"},600);
+        $('.box').animate({marginLeft: "5%"},600);
+        $('.wrapper').animate({marginTop: "3%"},600);
+});
+
+//   	$(".box").css("margin-left", "0px")
+//   	$(".wrapper").css("margin-top", "0px")
+//   	$('.box').animate({width:"100px", height:"90px"});
+//   	$('#alp').animate({width:"50%", height:"50%"});
+
+
+// });
+
+
+
+
+
+// $("#wrapper1").click(function(){
+//   	$("#al,#bl,#cl,#dl,#el").animate({
+// 			width: '0px',
+// 			}, 800);
+//   	$("#al,#bl,#cl,#dl,#el").animate({
+// 			width: '200px',
+// 			}, 3000);
+//  });
+
+// $("#wrapper2").click(function(){
+// 	$("#fl,#gl,#hl,#il,#jl").animate({
+// 			height: '0px',
+// 			}, 800);
+// 	$("#fl,#gl,#hl,#il,#jl").animate({
+// 			height: '180px',
+// 			}, 3000);
+//  });
+
+// $("#wrapper3").click(function(){
+// 	$("#kl,#ll,#ml,#nl,#ol").animate({
+// 			width: '0px',
+// 			}, 800);
+// 	$("#kl,#ll,#ml,#nl,#ol").animate({
+// 			width: '200px',
+// 			}, 3000);
+//  });
+
+// $("#wrapper4").click(function(){
+// 	$("#pl,#ql,#rl,#sl,#tl").animate({
+// 			height: '0px',
+// 			}, 800);
+// 	$("#pl,#ql,#rl,#sl,#tl").animate({
+// 			height: '180px',
+// 			}, 3000);
+//  });
+
+// $("#wrapper5").click(function(){
+// 	$("#ul,#vl,#wl,#xl,#yl").animate({
+// 			width: '0px',
+// 			}, 800);
+// 	$("#ul,#vl,#wl,#xl,#yl").animate({
+// 			width: '200px',
+// 			}, 3000);
+
+//  });
+
+// $("#wrapper6").click(function(){
+// 	$("#zl").animate({
+// 			height: '0px',
+// 			}, 800);
+// 	$("#zl").animate({
+// 			height: '180px',
+// 			}, 3000);
+
+//  });
+
+
+
+ //  	$("#al,#bl,#cl,#dl,#el").animate({
+	// 		width: '200px',
+	// 		}, 500);
+ //  	$("#fl,#gl,#hl,#il,#jl").animate({
+	// 		width: '200px',
+	// 		}, 1000);
+ //  	$("#kl,#ll,#ml,#nl,#ol").animate({
+	// 		width: '200px',
+	// 		}, 1500);
+ //  	$("#pl,#ql,#rl,#sl,#tl").animate({
+	// 		width: '200px',
+	// 		}, 2000);
+ //  	$("#ul,#vl,#wl,#xl,#yl").animate({
+	// 		width: '200px',
+	// 		}, 2500);
+ //  	$("#zl").animate({
+	// 		width: '200px',
+	// 		}, 3000);
+ // });
+
+
+$("#slider1").slideReveal({
+  trigger: $("#trigger1"),
+  autoEscape: true,
+  width: 1450,
+  speed: 1000,
+  position: "right"
+});
+
+
+
+$('#trigger3').click(function() {
+    location.reload();
+});
+
+
+
+
+
+//end of document//
+ });	
